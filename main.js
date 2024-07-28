12069de6e49588913e328eb3a8368bfbbc62856c
João Victor R. Ventura <joaovventura@protonmail.com>
2024-07-28 06:50:35 -0400

    now is a userscript and not a extension

--- main.js
+++ main.js
@@ -1,5 +1,15 @@
+// ==UserScript==
+// @name        New script youtube.com
+// @namespace   Violentmonkey Scripts
+// @match       https://www.youtube.com/watch?v=*&list=WL*
+// @grant       none
+// @version     1.0
+// @author      -
+// @description 7/28/2024, 6:42:02 AM
+// @run-at      document-start
+// ==/UserScript==
+
 url = window.location.href
 url_nova = url.substring(0,43)
 console.log(url_nova)
-//window.location.href = url_nova
 window.location.replace(url_nova)
